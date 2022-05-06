a63ddb113d58c9cbc06972ac765b892db43a798b
skatika007@gmail.com <skatika007@gmail.com>
2022-05-06 07:03:46 +0000

    hi

--- models/example/fileSP.sql
+++ models/example/fileSP.sql
@@ -7,9 +7,14 @@ with sales_test as (
 
 product_test as (
     select * from {{ ref('stage_product')}}
-)
+),
+
+SALES_PRODUCT AS(
 
 select p.batch,s.id,s.product,s.cost,s.delivery from sales_test s,product_test p
+
+)
+select * FROM SALES_PRODUCT;
 
 
 
