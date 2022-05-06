eccab71c033e7fc0bc4f9b72f9c7793606b2f868
skatika007@gmail.com <skatika007@gmail.com>
2022-05-06 06:57:37 +0000

    update

--- models/example/file.sql
+++ models/example/file.sql
@@ -12,7 +12,7 @@ product_test as (
     select * from {{ ref('stage_product')}}
 )
 
-select p.batch,s.id,s.product,s.cost from sales_test s,product_test p
+select p.batch,s.id,s.product,s.cost,s.delivery from sales_test s,product_test p
 
 
 
